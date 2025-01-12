3ee38463c64097663df640a1a03bc5e9b715b7ca
Debanjan110d <debanjangamedu@gmail.com>
2025-01-12 23:52:27 +0530

    starting

---
+++ tyoes_2.java
@@ -0,0 +1,22 @@
+public class tyoes_2 {
+    public static void main(String[] args) {
+        byte age =20;
+        short s = 20;
+        int i = 20;
+        long l = 20;
+        float f = 20;
+        double d = 20;
+        char c = 'a';
+        boolean b = true;
+        String s1 = "hello";
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);    
+        System.out.println(s1);
+    }
+}
