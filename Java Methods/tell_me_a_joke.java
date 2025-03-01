7c4946bb380cea5b4de8055917e2a1a6992c4511
Debanjan110d <debanjangamedu@gmail.com>
2025-03-01 23:59:31 +0530

    methods some update with tell me a joke code

---
+++ Java Methods/tell_me_a_joke.java
@@ -0,0 +1,25 @@
+public class tell_me_a_joke {
+    /**
+     * This method prints a simple joke to the console.
+     * It doesn't take any parameters and doesn't return any value.
+     */
+    public static void tellJoke() {
+        // Print the setup of the joke
+        System.out.println("Why don't scientists trust atoms?");
+
+        // Print the punchline of the joke
+        System.out.println("Because they make up everything!");
+    }
+
+    public static void main(String[] args) {
+        tellJoke(); // Call the method to print a joke
+        telljoke();
+    }
+
+    // We can mae it here also
+    public static void telljoke() {
+        System.out.println("Why don't eggs tell jokes?");
+
+        System.out.println("They'd crack each other up!");
+    }
+}
