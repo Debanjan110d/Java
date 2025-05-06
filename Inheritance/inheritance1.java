60c898e86bea2ee4adfbb792e7a920845f89d06f
Debanjan110d <debanjangamedu@gmail.com>
2025-05-06 22:28:17 +0530

    Inheritance Done

--- Inheritance/inheritance1.java
+++ Inheritance/inheritance1.java
@@ -17,16 +17,36 @@ class Base {
         System.out.println("I am a constructor");
     }
 
-    class Derived extends Base {
-        public void printMe() {
-            System.out.println("I am a derived constructor");
-        }
+}
+
+class Derived extends Base {
+    public int y;
+
+    public int gety() {
+        return y;
+    }
+
+    public void sety(int y) {
+        this.y = y;
+    }
+
+    public void printMe() {
+        System.out.println("I am a derived constructor");
     }
 }
 
 public class inheritance1 {
     public static void main(String[] args) {
         Base b = new Base();
+        b.setX(5);
+        System.out.println(b.getX());
         b.printMe();
+
+        Derived d = new Derived();
+        d.setX(10);
+        d.sety(20);
+        System.out.println(d.getX());
+        System.out.println(d.gety());
+        d.printMe();
     }
 }
